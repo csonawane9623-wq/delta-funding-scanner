1e2327c5436efc5bae30bd3a2370d0a8eb94d890
csonawane9623-wq <csonawane9623@gmail.com>
2025-12-26 15:27:39 +0530

    Update funding_bot.py

--- funding_bot.py
+++ funding_bot.py
@@ -12,7 +12,7 @@ BASE_URL = "https://api.india.delta.exchange"
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-ALERT_THRESHOLD = 0.8          # percentage (±0.8000%)
+ALERT_THRESHOLD = 0.0800       # percentage (±0.8000%)
 COOLDOWN_FILE = "last_alerts.json"
 
 session = requests.Session()
